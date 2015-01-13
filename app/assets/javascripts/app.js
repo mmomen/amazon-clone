5d7a41821c5e7ef6b7d977607d944cad5a5decfc
Mazdak Momen <mazdak.momen@gmail.com>
2015-01-12 18:37:57 -0800

    Added ability to remove items from cart. Made cart an object with two properties, an array of items and total price of cart

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,22 +3,35 @@ var app = angular.module('AmazonApp', ['ngResource']);
 app.controller('MainCtrl', ['$resource', '$scope', function($resource, $scope) {
   var Items = $resource('/items/:id', {id: '@id'});
   $scope.items = Items.query();
-  $scope.cart = [];
-  $scope.cartTotal = 0;
+  $scope.cart = {
+    items: [],
+    total: 0
+  };
 
   $scope.addToCart = function(item) {
     if (item.quantity === 0) {
       item.quantity = "WE AIN'T GO NO MO GTFO";
     } else if (typeof item.quantity !== "string") {
       item.quantity--;
-      $scope.cartTotal += item.price;
-      if ($scope.cart.indexOf(item) > -1) {
-        console.log('f');
+      $scope.cart.total += item.price;
+      if ($scope.cart.items.indexOf(item) > -1) {
         item.amountInCart++;
       } else {
-        $scope.cart.push(item);
+        $scope.cart.items.push(item);
         item.amountInCart = 1;
       }
+    }
+  };
+
+  $scope.removeFromCart = function(item) {
+    item.quantity++;
+    item.amountInCart--;
+    $scope.cart.total -= item.price;
+    if (item.amountInCart === 0) {
+       var idx = $scope.cart.items.indexOf(item);
+       if (idx > -1) {
+        $scope.cart.items.splice(idx, 1);
+       }
     }
   };
 }]);
