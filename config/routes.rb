11a5683546bb8754f8ca2709cccb40449cb92d6c
Mazdak Momen <mazdak.momen@gmail.com>
2015-01-12 16:21:16 -0800

    Added CSRF protection and set items route to respond with in json format

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resources :orders
 
   root 'items#home'
-  resources :items
+  resources :items, defaults: {format: :json}
+  # render json: @items
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
