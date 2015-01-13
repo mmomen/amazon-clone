e93b03fcdff986ea6f0fcb85768025c01420465e
Mazdak Momen <mazdak.momen@gmail.com>
2015-01-12 18:14:14 -0800

    Added ability to view items in cart (with quantity) and ability to track cart total

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -4,15 +4,21 @@ app.controller('MainCtrl', ['$resource', '$scope', function($resource, $scope) {
   var Items = $resource('/items/:id', {id: '@id'});
   $scope.items = Items.query();
   $scope.cart = [];
+  $scope.cartTotal = 0;
 
   $scope.addToCart = function(item) {
     if (item.quantity === 0) {
       item.quantity = "WE AIN'T GO NO MO GTFO";
-      // var idx = $scope.items.indexOf(item);
-      // $scope.items[idx].$delete();
     } else if (typeof item.quantity !== "string") {
       item.quantity--;
-      $scope.cart.push(item.id);
+      $scope.cartTotal += item.price;
+      if ($scope.cart.indexOf(item) > -1) {
+        console.log('f');
+        item.amountInCart++;
+      } else {
+        $scope.cart.push(item);
+        item.amountInCart = 1;
+      }
     }
   };
 }]);
