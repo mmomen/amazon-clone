2b17cabb41fd76112b873b3a318f82f906e4911d
Mazdak Momen <mazdak.momen@gmail.com>
2015-01-15 15:03:09 -0800

    Using angular routes. Home page now just shows items with nav bar. Cart route\page is not working correct atm

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,1 +1,16 @@
 var app = angular.module('AmazonApp', ['ngResource', 'ngRoute']);
+
+app.config(function($routeProvider) {
+  $routeProvider
+    .when("/", {
+      templateUrl: "/assets/templates/items.html",
+      controller: "ItemsCtrl"
+    })
+    .when("/cart", {
+      templateUrl: "/templates/cart.html",
+      controller: "CartCtrl"
+    })
+    .otherwise({
+      redirectTo: "/asdasd" //fail on purpose
+    });
+});
